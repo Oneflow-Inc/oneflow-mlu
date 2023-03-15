f0cb29c3d779b7d8de1eb74cb2c542ad7047d4a1
Jianhua Zheng <zhengjianhua@oneflow.org>
2023-03-15 11:18:34 +0800

    using CnnlTensorDescriptor

--- oneflow/cambricon/kernels/scalar_math_kernels.cpp
+++ oneflow/cambricon/kernels/scalar_math_kernels.cpp
@@ -17,7 +17,9 @@ limitations under the License.
 
 #include <cnnl.h>
 
+#include "oneflow/cambricon/cnnl/cnnl_tensor_descriptor.h"
 #include "oneflow/cambricon/ep/mlu_stream.h"
+#include "oneflow/cambricon/ep/mlu_util.h"
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/common/scalar.h"
 
@@ -28,47 +30,24 @@ enum class BinaryOpMLU {
   kMul,
 };
 
-#define CNNL_CHECK(val) CHECK(CNNL_STATUS_SUCCESS == val)
-
-static void CopyTensorShapeDimSize(const user_op::Tensor* in, std::vector<int>& dim) {
-  const auto shape = in->shape_view();
-  auto num_axes = shape.NumAxes();
-  dim.reserve(num_axes);
-  for (decltype(num_axes) i = 0; i < num_axes; ++i) {
-    dim.push_back(shape.At(i));
-  }
-}
-
 // TODO: support other data types
+// CNNL 1.15.2
 static void LaunchAddKernel(user_op::KernelComputeContext* ctx, Scalar src0,
-    const user_op::Tensor* in, user_op::Tensor* out) {
+                            const user_op::Tensor* in, user_op::Tensor* out) {
+  CHECK(in->shape_view().NumAxes() <= CNNL_DIM_MAX)
+      << "The number of dimensions is no more than CNNL_DIM_MAX";
   using DType = float;
-  // create cnnlTensorDescriptor_t
-  cnnlTensorDescriptor_t input_desc;
-  CNNL_CHECK(cnnlCreateTensorDescriptor(&input_desc));
-  using UniquePtr =
-    std::unique_ptr<cnnlTensorDescriptor_t, void(*)(cnnlTensorDescriptor_t*)>;
-  auto p_input_desc = UniquePtr(&input_desc, [] (cnnlTensorDescriptor_t* desc) {
-    CNNL_CHECK(cnnlDestroyTensorDescriptor(*desc));
-  });
-  // set tensor descriptor
-  std::vector<int> dim;
-  CopyTensorShapeDimSize(in, dim);
-  // TODO(Jianhua Zheng): support data type mapping
-  CNNL_CHECK(cnnlSetTensorDescriptor(
-    input_desc, CNNL_LAYOUT_ARRAY, CNNL_DTYPE_FLOAT, dim.size(), dim.data()));
-  // call Transform
+  CnnlTensorDescriptor input_desc;
+  input_desc.set(in);
   const DType alpha = 1;
   const DType beta = src0.Value<DType>();
   auto handle = ctx->stream()->As<ep::MluStream>()->cnnl_handle();
-  CNNL_CHECK(cnnlTransform(
-    handle, &alpha, input_desc, in->dptr(), &beta, out->mut_dptr()));
+  OF_CNNL_CHECK(
+      cnnlTransform(handle, &alpha, input_desc.desc(), in->dptr(), &beta, out->mut_dptr()));
 }
 
 // TODO: implement
-static void LaunchMulKernel() {
-
-}
+static void LaunchMulKernel() {}
 
 template<BinaryOpMLU op>
 class ScalarMathKernelMLU final : public user_op::OpKernel {
@@ -91,17 +70,13 @@ class ScalarMathKernelMLU final : public user_op::OpKernel {
     int64_t elem_cnt = out->shape_view().elem_cnt();
     if (elem_cnt != 0) {
       // TODO(Jianhua Zheng): support kSub
-      const bool is_add_sub_0 =
-          (op == BinaryOpMLU::kAdd)
-          && value.Value<double>() == 0.0;
+      const bool is_add_sub_0 = (op == BinaryOpMLU::kAdd) && value.Value<double>() == 0.0;
       // TODO(Jianhua Zheng): support kDiv
-      const bool is_mul_div_1 =
-          (op == BinaryOpMLU::kMul)
-          && value.Value<double>() == 1.0;
+      const bool is_mul_div_1 = (op == BinaryOpMLU::kMul) && value.Value<double>() == 1.0;
       if ((is_add_sub_0 || is_mul_div_1) && in->dptr() == out->dptr()) { return; }
       if (op == BinaryOpMLU::kAdd) {
         LaunchAddKernel(ctx, value, in, out);
-      } else if ( op == BinaryOpMLU::kMul) {
+      } else if (op == BinaryOpMLU::kMul) {
         LaunchMulKernel();
       }
     } else {
@@ -113,21 +88,21 @@ class ScalarMathKernelMLU final : public user_op::OpKernel {
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-#define SCALAR_MATH_SEQ                                           \
-  OF_PP_MAKE_TUPLE_SEQ("scalar_add", BinaryOpMLU::kAdd)           \
+#define SCALAR_MATH_SEQ                                 \
+  OF_PP_MAKE_TUPLE_SEQ("scalar_add", BinaryOpMLU::kAdd) \
   OF_PP_MAKE_TUPLE_SEQ("scalar_mul", BinaryOpMLU::kMul)
 
-#define REGISTER_UNARY_MATH_SCALAR_ELEMWISE_USER_KERNEL(op_name, binary_op)               \
-  REGISTER_USER_KERNEL(op_name)                                                           \
-      .SetCreateFn<ScalarMathKernelMLU<binary_op>>()                                      \
-      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kMLU)                     \
-                    && (user_op::HobDataType("in", 0) == user_op::HobDataType("out", 0))  \
-                    && (user_op::HobDataType("in", 0) == DataType::kFloat))               \
-      .SetInplaceProposalFn(                                                              \
-          [](const user_op::InferContext& ctx,                                            \
-             const user_op::AddInplaceArgPair& AddInplaceArgPairFn) -> Maybe<void> {      \
-            OF_RETURN_IF_ERROR(AddInplaceArgPairFn("out", 0, "in", 0, true));             \
-            return Maybe<void>::Ok();                                                     \
+#define REGISTER_UNARY_MATH_SCALAR_ELEMWISE_USER_KERNEL(op_name, binary_op)                 \
+  REGISTER_USER_KERNEL(op_name)                                                             \
+      .SetCreateFn<ScalarMathKernelMLU<binary_op>>()                                        \
+      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kMLU)                       \
+                       && (user_op::HobDataType("in", 0) == user_op::HobDataType("out", 0)) \
+                       && (user_op::HobDataType("in", 0) == DataType::kFloat))              \
+      .SetInplaceProposalFn(                                                                \
+          [](const user_op::InferContext& ctx,                                              \
+             const user_op::AddInplaceArgPair& AddInplaceArgPairFn) -> Maybe<void> {        \
+            OF_RETURN_IF_ERROR(AddInplaceArgPairFn("out", 0, "in", 0, true));               \
+            return Maybe<void>::Ok();                                                       \
           });
 
 OF_PP_FOR_EACH_TUPLE(REGISTER_UNARY_MATH_SCALAR_ELEMWISE_USER_KERNEL, SCALAR_MATH_SEQ)
--- python/oneflow/test/modules/test_scalar_add_cambricon.py
+++ python/oneflow/test/modules/test_scalar_add_cambricon.py
@@ -1,9 +1,12 @@
 """
 Copyright 2020 The OneFlow Authors. All rights reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,7 +25,9 @@ import oneflow.unittest
 
 
 def _test_scalar_add_forward(test_case, shape, device):
-    x = flow.tensor(np.random.randn(*shape), device=flow.device(device), dtype=flow.float)
+    x = flow.tensor(
+        np.random.randn(*shape), device=flow.device(device), dtype=flow.float
+    )
     y = np.random.randn()
     of_out = x + y
     np_out = np.add(x.numpy(), y)
