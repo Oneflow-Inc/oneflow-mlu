8bbed0c8a7b52e7c08bcfffa14eb90301a4c0e0f
luyang <flowingsun007@163.com>
2023-03-16 09:00:12 +0800

    format

--- oneflow/cambricon/kernels/normalization_kernel.cpp
+++ oneflow/cambricon/kernels/normalization_kernel.cpp
@@ -37,7 +37,7 @@ class MluNormalizationKernel final : public user_op::OpKernel {
   void Compute(user_op::KernelComputeContext* ctx) const override {
     const bool training = ctx->Attr<bool>("training");
     CHECK(!training);
-     
+
     const user_op::Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
     user_op::Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
     const auto* gamma = ctx->Tensor4ArgNameAndIndex("gamma", 0);
@@ -47,24 +47,27 @@ class MluNormalizationKernel final : public user_op::OpKernel {
     const auto axis = ctx->Attr<int32_t>("axis");
     CHECK_EQ(axis, x->shape_view().NumAxes() - 1);
     const auto epsilon = ctx->Attr<float>("epsilon");
-    
+
     CnnlTensorDescriptor input_desc, output_desc, weight_bias_mean_var_desc;
     input_desc.set(x);
     output_desc.set(y);
     weight_bias_mean_var_desc.set(gamma);
-    OF_CNNL_CHECK(cnnlBatchNormForwardInference(ctx->stream()->As<ep::MluStream>()->cnnl_handle(), nullptr, nullptr, input_desc.desc(), x->dptr(),
-                                           weight_bias_mean_var_desc.desc(), gamma->dptr(), beta->dptr(), moving_mean->dptr(),
-                                           moving_variance->raw_dptr(), epsilon, output_desc.desc(), y->mut_dptr()));
+    OF_CNNL_CHECK(cnnlBatchNormForwardInference(
+        ctx->stream()->As<ep::MluStream>()->cnnl_handle(), nullptr, nullptr, input_desc.desc(),
+        x->dptr(), weight_bias_mean_var_desc.desc(), gamma->dptr(), beta->dptr(),
+        moving_mean->dptr(), moving_variance->raw_dptr(), epsilon, output_desc.desc(),
+        y->mut_dptr()));
     ctx->stream()->Sync();
   }
 
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-#define REGISTER_RELU_MLU_KERNEL(dtype)                                              \
-  REGISTER_USER_KERNEL("normalization").SetCreateFn<MluNormalizationKernel<dtype>>().SetIsMatchedHob( \
-      (user_op::HobDeviceType() == DeviceType::kMLU)                                 \
-      && (user_op::HobDataType("x", 0) == GetDataType<dtype>::value));
+#define REGISTER_RELU_MLU_KERNEL(dtype)                               \
+  REGISTER_USER_KERNEL("normalization")                               \
+      .SetCreateFn<MluNormalizationKernel<dtype>>()                   \
+      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kMLU) \
+                       && (user_op::HobDataType("x", 0) == GetDataType<dtype>::value));
 
 REGISTER_RELU_MLU_KERNEL(float)
 REGISTER_RELU_MLU_KERNEL(float16)
