ca74ca93c4b3e6e32bdc83e6dcdd56a54332f8b4
youxiudeshouyeren <1929724847@qq.com>
2023-03-16 14:08:08 +0800

    fix

--- oneflow/user/kernels/layer_norm_cpu_kernel.cpp
+++ oneflow/user/kernels/layer_norm_cpu_kernel.cpp
@@ -18,63 +18,41 @@ limitations under the License.
 namespace oneflow {
 
 template<typename T>
-class LayerNormCpuKernel final : public user_op::OpKernel {
+class LayerNormMluKernel final : public user_op::OpKernel {
  public:
-  LayerNormCpuKernel() = default;
-  ~LayerNormCpuKernel() = default;
+  LayerNormMluKernel() = default;
+  ~LayerNormMluKernel() = default;
 
  private:
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
-  void Compute(user_op::KernelComputeContext* ctx) const override { TODO(); };
+  void Compute(user_op::KernelComputeContext* ctx) const override { 
+    const user_op::Tensor* in = ctx->Tensor4ArgNameAndIndex("x", 0);
+    user_op::Tensor* out = ctx->Tensor4ArgNameAndIndex("y", 0);
+    user_op::Tensor* mean = ctx->Tensor4ArgNameAndIndex("mean", 0);
+    user_op::Tensor* mean = ctx->Tensor4ArgNameAndIndex("inv_variance", 0);
+    const T* gamma_ptr = nullptr;
+    const T* beta_ptr = nullptr;
+    if (ctx->has_input("gamma", 0)) {
+      const user_op::Tensor* gamma = ctx->Tensor4ArgNameAndIndex("gamma", 0);
+      gamma_ptr = gamma->dptr<T>();
+      CHECK_EQ(gamma->shape_view().elem_cnt(), norm_size);
+    }
+    if (ctx->has_input("beta", 0)) { beta_ptr = ctx->Tensor4ArgNameAndIndex("beta", 0)->dptr<T>(); }
+
+
+
+  };
 };
 
-#define REGISTER_LAYER_NORM_CPU_KERNEL(dtype)                         \
+#define REGISTER_LAYER_NORM_MLU_KERNEL(dtype)                         \
   REGISTER_USER_KERNEL("layer_norm")                                  \
-      .SetCreateFn<LayerNormCpuKernel<dtype>>()                       \
-      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kCPU) \
+      .SetCreateFn<LayerNormMluKernel<dtype>>()                       \
+      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kMLU) \
                        && (user_op::HobDataType("x", 0) == GetDataType<dtype>::value));
 
-REGISTER_LAYER_NORM_CPU_KERNEL(float)
-REGISTER_LAYER_NORM_CPU_KERNEL(double)
+REGISTER_LAYER_NORM_MLU_KERNEL(float)
+REGISTER_LAYER_NORM_MLU_KERNEL(float16)
 
-template<typename T>
-class LayerNormGradCpuKernel final : public user_op::OpKernel {
- public:
-  LayerNormGradCpuKernel() = default;
-  ~LayerNormGradCpuKernel() = default;
 
- private:
-  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
-  void Compute(user_op::KernelComputeContext* ctx) const override { TODO(); };
-};
-
-#define REGISTER_LAYER_NORM_GRAD_CPU_KERNEL(dtype)                    \
-  REGISTER_USER_KERNEL("layer_norm_grad")                             \
-      .SetCreateFn<LayerNormGradCpuKernel<dtype>>()                   \
-      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kCPU) \
-                       && (user_op::HobDataType("dy", 0) == GetDataType<dtype>::value));
-
-REGISTER_LAYER_NORM_GRAD_CPU_KERNEL(float)
-REGISTER_LAYER_NORM_GRAD_CPU_KERNEL(double)
-
-template<typename T>
-class LayerNormParamGradCpuKernel final : public user_op::OpKernel {
- public:
-  LayerNormParamGradCpuKernel() = default;
-  ~LayerNormParamGradCpuKernel() = default;
-
- private:
-  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
-  void Compute(user_op::KernelComputeContext* ctx) const override { TODO(); };
-};
-
-#define REGISTER_LAYER_NORM_PARAM_GRAD_CPU_KERNEL(dtype)              \
-  REGISTER_USER_KERNEL("layer_norm_param_grad")                       \
-      .SetCreateFn<LayerNormParamGradCpuKernel<dtype>>()              \
-      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kCPU) \
-                       && (user_op::HobDataType("dy", 0) == GetDataType<dtype>::value));
-
-REGISTER_LAYER_NORM_PARAM_GRAD_CPU_KERNEL(float)
-REGISTER_LAYER_NORM_PARAM_GRAD_CPU_KERNEL(double)
 
 }  // namespace oneflow
