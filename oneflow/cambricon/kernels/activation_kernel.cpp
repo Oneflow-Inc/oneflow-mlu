829eb3d415aa656606480991740ef3b5b0eb49c2
youxiudeshouyeren <1929724847@qq.com>
2023-03-16 13:49:04 +0800

    refine

--- oneflow/cambricon/kernels/activation_kernel.cpp
+++ oneflow/cambricon/kernels/activation_kernel.cpp
@@ -30,7 +30,7 @@ void SetCnnlActivationDescriptor(CnnlActivationDescriptor* activation_desc) {
                        /*nanProp=*/CNNL_NOT_PROPAGATE_NAN, /*ceof=*/1.0);
 }
 
-template<typename T, cnnlActivationMode_t mode>
+template<typename T, cnnlActivationMode_t mode, const char* input_name, const char* output_name>
 class MluActivationKernel final : public user_op::OpKernel {
  public:
   MluActivationKernel() = default;
@@ -40,8 +40,8 @@ class MluActivationKernel final : public user_op::OpKernel {
   using user_op::OpKernel::Compute;
 
   void Compute(user_op::KernelComputeContext* ctx) const override {
-    const user_op::Tensor* in = ctx->Tensor4ArgNameAndIndex("x", 0);
-    user_op::Tensor* out = ctx->Tensor4ArgNameAndIndex("y", 0);
+    const user_op::Tensor* in = ctx->Tensor4ArgNameAndIndex(input_name, 0);
+    user_op::Tensor* out = ctx->Tensor4ArgNameAndIndex(output_name, 0);
 
     CnnlTensorDescriptor input_desc, output_desc;
     input_desc.set(in);
@@ -56,12 +56,19 @@ class MluActivationKernel final : public user_op::OpKernel {
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-#define REGISTER_ACTIVATION_MLU_KERNEL(name, mode, dtype)                                     \
-  REGISTER_USER_KERNEL(name).SetCreateFn<MluActivationKernel<dtype, mode>>().SetIsMatchedHob( \
-      (user_op::HobDeviceType() == DeviceType::kMLU)                                          \
-      && (user_op::HobDataType("x", 0) == GetDataType<dtype>::value));
+#define REGISTER_ACTIVATION_MLU_KERNEL(name, mode, dtype, input_name, output_name) \
+  REGISTER_USER_KERNEL(name)                                                       \
+      .SetCreateFn<MluActivationKernel<dtype, mode, input_name, output_name>>()    \
+      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kMLU)              \
+                       && (user_op::HobDataType(input_name, 0) == GetDataType<dtype>::value));
 
-REGISTER_ACTIVATION_MLU_KERNEL("relu", CNNL_ACTIVATION_RELU, float)
-REGISTER_ACTIVATION_MLU_KERNEL("relu", CNNL_ACTIVATION_RELU, float16)
+const char x_str[] = "x";
+const char y_str[] = "y";
+const char in_str[] = "in";
+const char out_str[] = "out";
+REGISTER_ACTIVATION_MLU_KERNEL("relu", CNNL_ACTIVATION_RELU, float, x_str, y_str)
+REGISTER_ACTIVATION_MLU_KERNEL("relu", CNNL_ACTIVATION_RELU, float16, x_str, y_str)
+REGISTER_ACTIVATION_MLU_KERNEL("gelu", CNNL_ACTIVATION_GELU, float, in_str, out_str)
+REGISTER_ACTIVATION_MLU_KERNEL("gelu", CNNL_ACTIVATION_GELU, float16, in_str, out_str)
 
 }  // namespace oneflow
