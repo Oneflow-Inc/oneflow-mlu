4585b79577f0abca57e29089b7a9fcd791e57fd5
BBuf <1182563586@qq.com>
2023-03-17 15:56:44 +0800

    fix memory allocate bug

--- oneflow/cambricon/kernels/adaptive_avg_pool_kernel.cpp
+++ oneflow/cambricon/kernels/adaptive_avg_pool_kernel.cpp
@@ -46,21 +46,30 @@ class AdaptiveAvgPool2DKernel final : public user_op::OpKernel {
     const T* in_ptr = in_tensor->dptr<T>();
     T* out_ptr = out_tensor->mut_dptr<T>();
 
-    size_t tmp_in_workspace_size = in_tensor->shape_view().elem_cnt();
+    size_t tmp_in_workspace_size =
+        in_tensor->shape_view().elem_cnt() * sizeof(in_tensor->data_type());
     CnnlWorkspace tmp_in_cnnl_workspace(ctx->stream()->As<ep::MluStream>(), tmp_in_workspace_size);
     void* tmp_in_ptr = tmp_in_cnnl_workspace.dptr();
 
-    std::vector<int64_t> in_shapevec(
-          {in_tensor->shape_view().At(0), in_tensor->shape_view().At(2), in_tensor->shape_view().At(3), in_tensor->shape_view().At(1)});
+    std::vector<int64_t> in_shapevec({in_tensor->shape_view().At(0), in_tensor->shape_view().At(1),
+                                      in_tensor->shape_view().At(2),
+                                      in_tensor->shape_view().At(3)});
     auto transpose = NewPermutePrimitive(ctx, in_tensor->shape_view().NumAxes());
     CHECK(transpose);
     transpose->Launch(ctx->stream(), in_tensor->data_type(), in_tensor->shape_view().NumAxes(),
-                    in_shapevec.data(), in_ptr,
-                    std::vector<int>({0, 2, 3, 1}).data(), tmp_in_ptr);
+                      in_shapevec.data(), in_ptr, std::vector<int>({0, 2, 3, 1}).data(),
+                      tmp_in_ptr);
+    ctx->stream()->As<ep::MluStream>()->Sync();
 
     cnnlTensorDescriptor_t in_desc = nullptr, out_decs = nullptr;
-    const int in_dims[4] = {static_cast<int>(in_tensor->shape_view().At(0)), static_cast<int>(in_tensor->shape_view().At(2)), static_cast<int>(in_tensor->shape_view().At(3)), static_cast<int>(in_tensor->shape_view().At(1))};
-    const int out_dims[4] = {static_cast<int>(out_tensor->shape_view().At(0)), static_cast<int>(out_tensor->shape_view().At(2)), static_cast<int>(out_tensor->shape_view().At(3)), static_cast<int>(out_tensor->shape_view().At(1))};
+    const int in_dims[4] = {static_cast<int>(in_tensor->shape_view().At(0)),
+                            static_cast<int>(in_tensor->shape_view().At(2)),
+                            static_cast<int>(in_tensor->shape_view().At(3)),
+                            static_cast<int>(in_tensor->shape_view().At(1))};
+    const int out_dims[4] = {static_cast<int>(out_tensor->shape_view().At(0)),
+                             static_cast<int>(out_tensor->shape_view().At(2)),
+                             static_cast<int>(out_tensor->shape_view().At(3)),
+                             static_cast<int>(out_tensor->shape_view().At(1))};
     auto dtype = ConvertToCnnlDataType(in_tensor->data_type());
     cnnlTensorLayout_t layout = CNNL_LAYOUT_NHWC;
     OF_CNNL_CHECK(cnnlCreateTensorDescriptor(&in_desc));
@@ -69,40 +78,41 @@ class AdaptiveAvgPool2DKernel final : public user_op::OpKernel {
     OF_CNNL_CHECK(cnnlSetTensorDescriptor(out_decs, layout, dtype, 4, out_dims));
 
     size_t _adaptive_avg_pool2d_workspace_size = 0;
-    OF_CNNL_CHECK(cnnlGetAdaptivePoolingForwardWorkspaceSize(ctx->stream()->As<ep::MluStream>()->cnnl_handle(),
-                                          in_desc, CNNL_POOLING_AVERAGE_COUNT_INCLUDE_PADDING, out_decs,
-                                          &_adaptive_avg_pool2d_workspace_size));
-    CnnlWorkspace adaptive2d_cnnl_workspace(ctx->stream()->As<ep::MluStream>(), _adaptive_avg_pool2d_workspace_size);
+    OF_CNNL_CHECK(cnnlGetAdaptivePoolingForwardWorkspaceSize(
+        ctx->stream()->As<ep::MluStream>()->cnnl_handle(), in_desc,
+        CNNL_POOLING_AVERAGE_COUNT_INCLUDE_PADDING, out_decs,
+        &_adaptive_avg_pool2d_workspace_size));
+    CnnlWorkspace adaptive2d_cnnl_workspace(ctx->stream()->As<ep::MluStream>(),
+                                            _adaptive_avg_pool2d_workspace_size);
     void* _adaptive_avg_pool2d_workspace = adaptive2d_cnnl_workspace.dptr();
-    size_t tmp_out_workspace_size = out_tensor->shape_view().elem_cnt();
-    CnnlWorkspace tmp_out_cnnl_workspace(ctx->stream()->As<ep::MluStream>(), tmp_out_workspace_size);
+    size_t tmp_out_workspace_size =
+        out_tensor->shape_view().elem_cnt() * sizeof(in_tensor->data_type());
+    CnnlWorkspace tmp_out_cnnl_workspace(ctx->stream()->As<ep::MluStream>(),
+                                         tmp_out_workspace_size);
     void* tmp_out_ptr = tmp_out_cnnl_workspace.dptr();
-    OF_CNNL_CHECK(cnnlAdaptivePoolingForward_v2(ctx->stream()->As<ep::MluStream>()->cnnl_handle(), 
-                                                in_desc, 
-                                                tmp_in_ptr, 
-                                                CNNL_POOLING_AVERAGE_COUNT_INCLUDE_PADDING, 
-                                                _adaptive_avg_pool2d_workspace,
-                                                _adaptive_avg_pool2d_workspace_size,
-                                                out_decs,
-                                                tmp_out_ptr,
-                                                NULL,
-                                                NULL));
+    OF_CNNL_CHECK(cnnlAdaptivePoolingForward_v2(
+        ctx->stream()->As<ep::MluStream>()->cnnl_handle(), in_desc, tmp_in_ptr,
+        CNNL_POOLING_AVERAGE_COUNT_INCLUDE_PADDING, _adaptive_avg_pool2d_workspace,
+        _adaptive_avg_pool2d_workspace_size, out_decs, tmp_out_ptr, NULL, NULL));
+    ctx->stream()->As<ep::MluStream>()->Sync();
     std::vector<int64_t> out_shapevec(
-          {out_tensor->shape_view().At(0), out_tensor->shape_view().At(1), out_tensor->shape_view().At(2), out_tensor->shape_view().At(3)});
+        {out_tensor->shape_view().At(0), out_tensor->shape_view().At(2),
+         out_tensor->shape_view().At(3), out_tensor->shape_view().At(1)});
     transpose = NewPermutePrimitive(ctx, out_tensor->shape_view().NumAxes());
     CHECK(transpose);
-    transpose->Launch(ctx->stream(), in_tensor->data_type(), in_tensor->shape_view().NumAxes(),
-                    out_shapevec.data(), tmp_out_ptr,
-                    std::vector<int>({0, 3, 1, 2}).data(), out_ptr);
+    transpose->Launch(ctx->stream(), out_tensor->data_type(), out_tensor->shape_view().NumAxes(),
+                      out_shapevec.data(), tmp_out_ptr, std::vector<int>({0, 3, 1, 2}).data(),
+                      out_ptr);
   }
 
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-#define REGISTER_ADAPTIVE_AVGPOOL2D_MLU_KERNEL(dtype)                                             \
-  REGISTER_USER_KERNEL("adaptive_avg_pool2d").SetCreateFn<AdaptiveAvgPool2DKernel<dtype>>().SetIsMatchedHob( \
-      (user_op::HobDeviceType() == DeviceType::kMLU)                                \
-      && (user_op::HobDataType("x", 0) == GetDataType<dtype>::value));
+#define REGISTER_ADAPTIVE_AVGPOOL2D_MLU_KERNEL(dtype)                 \
+  REGISTER_USER_KERNEL("adaptive_avg_pool2d")                         \
+      .SetCreateFn<AdaptiveAvgPool2DKernel<dtype>>()                  \
+      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kMLU) \
+                       && (user_op::HobDataType("x", 0) == GetDataType<dtype>::value));
 
 REGISTER_ADAPTIVE_AVGPOOL2D_MLU_KERNEL(float)
 REGISTER_ADAPTIVE_AVGPOOL2D_MLU_KERNEL(float16)
--- python/oneflow/test/cambricon/test_adaptive_avg_pool2d.py
+++ python/oneflow/test/cambricon/test_adaptive_avg_pool2d.py
@@ -24,16 +24,12 @@ import oneflow as flow
 import oneflow.unittest
 
 
-def _test_adaptive_avg_pool2d_forward(test_case, shape, out_shape,  device, dtype):
+def _test_adaptive_avg_pool2d_forward(test_case, shape, out_shape, device, dtype):
     x = flow.tensor(np.random.randn(*shape), device=flow.device(device), dtype=dtype)
     pool = flow.nn.AdaptiveAvgPool2d((out_shape[2], out_shape[3]))
     y = pool(x)
     y_cpu = pool(x.to("cpu"))
-    print(x.numpy())
-    print(y.numpy().flatten())
-    print(y_cpu.numpy().flatten())
     test_case.assertTrue(np.allclose(y.numpy(), y_cpu.numpy(), 0.0001, 0.0001))
-
 
 
 @flow.unittest.skip_unless_1n1d()
@@ -43,14 +39,15 @@ class TestAdaptiveAvgPool2dCambriconModule(flow.unittest.TestCase):
         arg_dict["test_fun"] = [
             _test_adaptive_avg_pool2d_forward,
         ]
-        arg_dict["shape"] = [(1, 2, 2, 2), (1, 3, 128, 128)]
-        arg_dict["out_shape"] = [(1, 2, 1, 1), (1, 3, 32, 35)]
+        arg_dict["shape"] = [(1, 2, 224, 224), (1, 3, 128, 128)]
+        arg_dict["out_shape"] = [(1, 2, 64, 64), (1, 3, 32, 35)]
         arg_dict["device"] = ["mlu"]
         arg_dict["dtype"] = [
             flow.float32,
-            flow.float16,
         ]
         for arg in GenArgList(arg_dict):
             arg[0](test_case, *arg[1:])
+
+
 if __name__ == "__main__":
     unittest.main()
