9905eef1cefeaee1de1010e49b7e32608dbe74b3
Ldpe2G <liangdepeng@gmail.com>
2023-03-21 14:18:06 +0800

    fix bug

--- oneflow/cambricon/ep/primitive/copy_nd.cpp
+++ oneflow/cambricon/ep/primitive/copy_nd.cpp
@@ -52,7 +52,7 @@ class CopyNdImpl : public CopyNd {
 
         int src_offset = src_pos[num_dims - 1];
         int dst_offset = dst_pos[num_dims - 1];
-        for (int i = num_dims - 2; i > 0; --i) {
+        for (int i = num_dims - 2; i >= 0; --i) {
           src_stride[i] = src_stride[i + 1] * static_cast<int>(src_dims[i + 1]);
           dst_stride[i] = dst_stride[i + 1] * static_cast<int>(dst_dims[i + 1]);
           src_offset += static_cast<int>(src_pos[i]) * src_stride[i];
