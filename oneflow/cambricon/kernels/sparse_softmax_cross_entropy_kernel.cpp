636fde362e227136d10214ba9b2b9446bfb865d5
youxiudeshouyeren <1929724847@qq.com>
2023-03-21 21:35:04 +0800

    fmt

--- oneflow/cambricon/kernels/sparse_softmax_cross_entropy_kernel.cpp
+++ oneflow/cambricon/kernels/sparse_softmax_cross_entropy_kernel.cpp
@@ -13,14 +13,15 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+#include "oneflow/core/kernel/kernel_util.h"
 #include "oneflow/cambricon/cnnl/cnnl_workspace.h"
 #include "oneflow/cambricon/common/mlu_util.h"
 #include "oneflow/cambricon/ep/mlu_stream.h"
 #include "oneflow/cambricon/cnnl/cnnl_tensor_descriptor.h"
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/kernel/new_kernel_util.h"
-#include "oneflow/core/kernel/kernel_util.h"
-#include "oneflow/core/ep/include/primitive/fill.h"
+
+
 namespace oneflow {
 
 class MluSparseSoftmaxCrossEntropyKernel final : public user_op::OpKernel {
