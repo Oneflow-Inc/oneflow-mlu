c3928a5a1b59090dd5c358387dfc546ca4596f95
hjchen2 <chenhoujiangcug@gmail.com>
2023-03-24 23:04:53 +0800

    refine

--- oneflow/cambricon/bang/bang_handle.h
+++ oneflow/cambricon/bang/bang_handle.h
@@ -13,20 +13,24 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#ifndef ONEFLOW_CAMBRICON_BANG_BANG_KERNELS_H_
-#define ONEFLOW_CAMBRICON_BANG_BANG_KERNELS_H_
+#ifndef ONEFLOW_CAMBRICON_BANG_BANG_HANDLE_H_
+#define ONEFLOW_CAMBRICON_BANG_BANG_HANDLE_H_
 
-#include "oneflow/cambricon/bang/bang_handle.h"
+#include <stdint.h>
+
+#include "cnrt.h"
 
 namespace oneflow {
 
-// input is a 3D tensor with shape is [batch, N, length]
-// indices is a 1D tensor with shape is [index_size]
-template<typename T, typename K>
-void bang_gather_kernel(BangHandle& handle, const T* input, int64_t batch, int64_t N,
-                        int64_t length, const K* index, int64_t index_size, T* output,
-                        int64_t offset);
+typedef struct BangHandle {
+  cnrtQueue_t queue;
+  uint32_t nclusters;
+  uint32_t ncores_per_cluster;
+
+  BangHandle(cnrtQueue_t q, int nclusters, int ncores)
+      : queue(q), nclusters(nclusters), ncores_per_cluster(ncores) {}
+} BangHandle;
 
 }  // namespace oneflow
 
-#endif  // ONEFLOW_CAMBRICON_BANG_BANG_KERNELS_H_
+#endif  // ONEFLOW_CAMBRICON_BANG_BANG_HANDLE_H_
