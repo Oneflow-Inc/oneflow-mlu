3b0e0385e30dc2f46c8a1e5b09dd9453acdf0ab5
mosout <mosout@qq.com>
2023-03-27 18:17:13 +0800

    fix

--- oneflow/cambricon/kernels/multi_reduce_kernels.cpp
+++ oneflow/cambricon/kernels/multi_reduce_kernels.cpp
@@ -120,13 +120,14 @@ class MluMultiReduceSumPowAbsKernel final : public user_op::OpKernel,
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-#define REGISTER_MLU_MULTI_REDUCE_SUM_POW_ABS_KERNEL(dtype)       \
+#define REGISTER_MLU_MULTI_REDUCE_SUM_POW_ABS_KERNEL(dtype)           \
   REGISTER_USER_KERNEL("multi_reduce_sum_pow_abs")                    \
       .SetCreateFn<MluMultiReduceSumPowAbsKernel<dtype>>()            \
       .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kMLU) \
                        && (user_op::HobDataType("y", 0) == GetDataType<dtype>::value));
 
 REGISTER_MLU_MULTI_REDUCE_SUM_POW_ABS_KERNEL(float)
-REGISTER_MLU_MULTI_REDUCE_SUM_POW_ABS_KERNEL(double)
+// cast primitive doesn't support convertingdouble to float
+// REGISTER_MLU_MULTI_REDUCE_SUM_POW_ABS_KERNEL(double)
 
 }  // namespace oneflow
